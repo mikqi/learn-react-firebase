777b92d6a5956b3cffebc2a24cc643524ed0ec00
Muhammad Rivki <muhammad.rivki@outlook.com>
2018-03-13 23:21:11 +0700

    [add] sample data chat

--- src/components/CardMessage.js
+++ src/components/CardMessage.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 export default (props) => (
   <div className="card p-3 mt-2 text-muted" style={{maxWidth: '60%'}}>
-    hellow
+    <div className="small">{props.name}</div>
+    <span>{props.message}</span>
   </div>
 );
--- src/components/MessagesContainer.js
+++ src/components/MessagesContainer.js
@@ -1,13 +1,27 @@
 import React, {Component} from 'react';
 import CardMessage from './CardMessage'
 
+const dummyChat = [
+  {
+    name: 'Unyil',
+    message: 'Hello guys apa kabar?'
+  }, {
+    name: 'Usro',
+    message: 'Heyy unyil apa kabar?'
+  }, {
+    name: 'Unyil',
+    message: 'Ehh. usro. Baik sro.'
+  }, {
+    name: 'Pak Ogah',
+    message: 'Ehh. Bagi cepe dong den.'
+  }
+]
+
 export default class MessagesContainer extends Component {
   render() {
     return (
       <div className="p-3">
-        <CardMessage />
-        <CardMessage />
-        <CardMessage />
+        {dummyChat.map(chat => <CardMessage message={chat.message} name={chat.name} />)}
       </div>
     );
   }
